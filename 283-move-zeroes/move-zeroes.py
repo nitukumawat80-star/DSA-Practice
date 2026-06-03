ecd811621030eb9fba6b02ac4167b45a4ec64ae4
Nitesh Kumawat <nitukumawat80@gmail.com>
2026-06-03 18:32:07 +0530

    Time: 7 ms (57.22%) | Memory: 13.5 MB (47.93%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -8,5 +8,5 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[k] , nums[i] = nums[i] , nums[k]
-                k+=1
-       
+
+                k += 1
