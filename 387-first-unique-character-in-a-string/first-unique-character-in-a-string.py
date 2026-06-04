019a286811957c0a3bacc1b43e726a064e885200
Nitesh Kumawat <nitukumawat80@gmail.com>
2026-06-04 13:37:14 +0530

    Time: 67 ms (95.93%) | Memory: 12.7 MB (64.75%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -5,16 +5,15 @@ class Solution(object):
         :rtype: int
         """
 
-        count ={}
+        count = defaultdict(int)
 
         for c in s:
-            count[c] = count.get(c , 0) + 1
+            count[c] += 1
 
-        for i in range(len(s)):
-            if count[s[i]] == 1:
+        for i , c in enumerate(s):
+            if count[c] == 1:
                 return i
-
-        return -1
-                 
+            
+        return -1         
 
         
