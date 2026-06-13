6efcf6a9d293118199be114d24a84c3ef84ca23f
Nitesh Kumawat <nitukumawat80@gmail.com>
2026-06-13 19:05:40 +0530

    Time: 4 ms (42.15%) | Memory: 12.6 MB (37.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,18 +1,25 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        brac = {")" : "(","}":"{","]":"["}
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bic = {')' : '(' , '}' : '{' , ']' : '['}
         s1 = []
         for i in s:
-            if i in "({[":
+            if i in "([{":
                 s1.append(i)
+
             else:
-                if len(s1) > 0 and s1[-1] == brac[i]:
+                if len(s1) > 0 and s1[-1] == bic[i]:
                     s1.pop()
+
                 else:
                     s1.append(i)
+
+
         if not s1:
             return True
+
         else:
             return False
-
-        
