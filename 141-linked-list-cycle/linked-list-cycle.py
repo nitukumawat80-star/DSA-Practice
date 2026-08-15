db94a5fe0adef0368890bbcfb44ad5e005e6f0db
Nitesh Kumawat <nitukumawat80@gmail.com>
2026-08-15 16:05:25 +0530

    Time: 33 ms (78.62%) | Memory: 19.5 MB (65.71%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,21 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-       
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
         slow = fast = head
 
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
+            fast = fast.next.next
 
-            if slow is fast:
+            if slow == fast:
                 return True
 
-
         return False
-        
